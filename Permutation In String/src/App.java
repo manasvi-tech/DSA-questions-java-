db732e088339c5b3be503e2787d5edab6c3cc0ab
Manasvi Arora <manasviarora28@gmail.com>
2024-03-17 23:48:46 +0530

    Permutation In String

---
+++ Permutation In String/src/App.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class App {
+    public static boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int[] freq = new int[26];
+        int m = s2.length();
+        for (int i = 0; i < n; i++) {
+            freq[s1.charAt(i) - 'a']++;
+        }
+        int[] freq2 = new int[26];
+        for (int i = 0; i < m; i++) {
+            freq2[s2.charAt(i) - 'a']++;
+            // Removing the last character while moving forward..good strategyyy
+            if (i >= n) {
+                freq2[s2.charAt(i - n) - 'a']--;
+            }
+            if (Arrays.equals(freq, freq2))
+                return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) throws Exception {
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+        System.out.println(checkInclusion(s1,s2));
+    }
+}
