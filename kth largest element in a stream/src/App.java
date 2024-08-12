8afc71bb9ab992465b2539f7c667778b12b974a9
manasvi-tech <manasviarora28@gmail.com>
2024-08-12 22:32:28 +0530

    kth largest

---
+++ kth largest element in a stream/src/App.java
@@ -0,0 +1,32 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class App {
+    class KthLargest {
+        int k;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(); // minHeap
+
+        public KthLargest(int k, int[] nums) {
+            this.k = k;
+            for (int num : nums) {
+                queue.add(num);
+
+                if (queue.size() > k) {
+                    queue.poll();
+                }
+            }
+        }
+
+        public int add(int val) {
+            queue.add(val);
+            if (queue.size() > k) {
+                queue.poll();
+            }
+            return queue.peek();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
