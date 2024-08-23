2ea92618baaf27bf164c3cd1057dd69de341d837
manasvi-tech <manasviarora28@gmail.com>
2024-08-23 19:47:30 +0530

    fractionaddition

---
+++ Fractional Addition and Subtraction/src/App.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class App {
+    public static String fractionAddition(String expression) {
+        int num=0;
+        int den=1;
+        int n = expression.length();
+        int i=0;
+        while(i<n){
+            int curNum=0;
+            int curDen=0;
+            boolean isNeg = false;
+            char ch = expression.charAt(i);
+            if(ch == '+' || ch == '-'){
+                if(ch=='-'){
+                    isNeg = true;
+                }
+                i++;
+            }
+            //form the num
+            int start=i;
+            while(Character.isDigit(expression.charAt(i))){
+                i++;
+            }
+            curNum = Integer.parseInt(expression.substring(start,i));
+            if(isNeg) curNum*=-1;
+            i++; //skip /
+            //form the den
+            start=i;
+            while(i<n && Character.isDigit(expression.charAt(i))){
+                i++;
+            }
+            curDen = Integer.parseInt(expression.substring(start,i));
+            num = num * curDen + curNum * den;
+            den *= curDen;
+        }
+        int gcd = Math.abs(getGCD(num,den));
+        num/=gcd;
+        den/=gcd;
+        return num + "/" + den;
+    }
+    public static int getGCD(int a, int b){
+        if(a==0) return b;
+        return getGCD(b%a,a);
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        System.out.println(fractionAddition(s));
+    }
+}
